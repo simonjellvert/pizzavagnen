d801b5e287e79780f4ee31b574377e5e69803ee4
simonjellvert <>
2023-11-10 14:16:55 +0000

    add html files

--- events/admin.py
+++ events/admin.py
@@ -7,4 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
--- events/views.py
+++ events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    template_name = 'about.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            status=1, event_time__gte=timezone.now()).order_by('_event_date')
