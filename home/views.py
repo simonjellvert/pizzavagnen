797fdc3c3aa9f39cb6347ff2403f9cde068a12b6
simonjellvert <>
2023-11-14 10:53:27 +0000

    Add homepage to urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['home_page_image'] = '/static/img/333064805_760223689005819_9082627831218226945_n.jpg'
+        return context
