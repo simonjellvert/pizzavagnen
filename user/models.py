a642d2495eb34d143d641abba8844413c7fc8f57
simonjellvert <>
2023-11-23 13:44:04 +0000

    Add content to models.py in user directory

--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    username = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = email
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return str(self.username)
