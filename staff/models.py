abba31fc8a9b3be28d5b734fa8899ea00f192a2e
simonjellvert <>
2023-11-28 11:46:20 +0000

    Add booking and staff apps

---
+++ staff/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+from booking.models import Booking
+
+
+class StaffArea(models.Model):
+    booking = models.ForeignKey(
+        Booking,
+        on_delete=models.CASCADE,
+        related_name='staff_bookings'
+    )
+    first_name = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='staff_fname',
+    )
+    last_name = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='staff_lname',
+    )
+    email = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='staff_email',
+    )
+    booking_date = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='staff_date',
+    )
+    booking_time = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='staff_time',
+    )
+
+    confirm_booking = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.booking} | {self.last_name}'
