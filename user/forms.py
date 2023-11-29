ff48e8bbc0866c8ae62fc2097826b8c5c9ba644a
simonjellvert <>
2023-11-29 14:25:42 +0000

    Edit user app

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
@@ -7,4 +7,25 @@ class SignUpForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
+
+
+class EditUserForm(UserChangeForm):
+
+    password = None
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email'
+        ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from .forms import SignUpForm, EditUserForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def register(request):
@@ -16,4 +18,28 @@ def register(request):
 
 
 def user_view(request):
-    return render(request, 'user/user.html')
+    return render(request, 'user/account.html')
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = EditUserForm(request.POST, instance=request.user)
+        password_form = PasswordChangeForm(request.user, request.POST)
+
+        if user_form.is_valid() and password_form.is_valid():
+            user_form.save()
+            password_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('user_view')
+        else:
+            messages.error(request, 'Please correct the error below')
+
+    else:
+        user_form = EditUserForm(instance=request.user)
+        password_form = PasswordChangeForm(request.user)
+
+    return render(request, 'user/account.html', {
+        'user_form': user_form,
+        'password_form': password_form
+    })
