4d8efcfa7b07cd1f5554d9153da58c630d522583
simonjellvert <>
2023-12-01 13:27:11 +0000

    Edit urls

--- pizzavagnen/urls.py
+++ pizzavagnen/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from events.views import PostList
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +27,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('user/', include('user.urls')),
     path('review/', include('review.urls')),
-]
-
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
