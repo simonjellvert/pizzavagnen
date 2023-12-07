0e5789d6cf66e66a0016615bf262a9af8c566d86
simonjellvert <>
2023-12-07 06:52:42 +0000

    Edit events/model

--- events/models.py
+++ events/models.py
@@ -26,8 +26,6 @@ class Post(models.Model):
         'Image',
         default='placeholder',
     )
-    updated_on = models.DateTimeField(auto_now=True)
-    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
